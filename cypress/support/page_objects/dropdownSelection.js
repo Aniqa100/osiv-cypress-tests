a243223be3b7e92901f0807171f07a83fce647ef
Aniqa100 <anyutka.vs@gmail.com>
2022-10-19 17:55:37 +0300

    remove akid form title
    Signed-off-by: Aniqa100 <anyutka.vs@gmail.com>

--- cypress/support/page_objects/dropdownSelection.js
+++ cypress/support/page_objects/dropdownSelection.js
@@ -4,29 +4,31 @@ export class DropdownSelection{
     adressTypeValue(){
 
     cy.get('[class="dhxwin_active"][modalwindow="true"]')
-    .find('[akid="sAdresseDetailOverviewForm-adresstyp"]').click().type('024').wait(500).get('[class="select2-results__options"]').find('[akid="adresstyp-A05:111:000000238504"]').click();
+    .find('[akid="sAdresseDetailOverviewForm-adresstyp"]').click().type('024').wait(500)
+    .get('[class="select2-results__options"]').find('[akid="adresstyp-A05:111:000000238504"]').click();
     
     }
 
     languageTypeValue(){
 
     cy.get('[class="dhxwin_active"][modalwindow="true"]')
-    .find('[akid="sAdresseDetailOverviewForm-sprache_bez"]').click().type('D').wait(100).get('[class="select2-results__options"]').click();
+    .find('[akid="sAdresseDetailOverviewForm-sprache_bez"]').click().type('D').wait(100)
+    .get('[class="select2-results__options"]').click();
 
     }
 
     salutationValue(){
      
-        cy.get('[class="dhxwin_active"][modalwindow="true"]').find('[akid="sAdresseDetailOverviewForm-anredeartbez"]')
+    cy.get('[class="dhxwin_active"][modalwindow="true"]').find('[akid="sAdresseDetailOverviewForm-anredeartbez"]')
     .click('top').get('[class="select2-search select2-search--dropdown"]').type('Fr')
     .get('[class="select2-results"]').click();
     
  }
 
     titleValue(){
-        cy.get('[class="dhxwin_active"][modalwindow="true"]').find('[akid="sAdresseDetailOverviewForm-titel_adresstitel"]')
+    cy.get('[class="dhxwin_active"][modalwindow="true"]').find('[akid="sAdresseDetailOverviewForm-titel_adresstitel"]')
     .click('top').get('[class="select2-search select2-search--dropdown"]').type('Dr')
-    .get('[class="select2-results__options"]').wait(100).find('[akid="titel_adresstitel-A6D:111:000005900065"]').click();
+    .get('[class="select2-results__options"]').click();
     }
 
     postalcodeValue(){
@@ -40,3 +42,7 @@ export class DropdownSelection{
 }
 
 export const dropdownValue = new DropdownSelection()
+
+
+
+//find('[akid="titel_adresstitel-A6D:111:000005900065"]')
