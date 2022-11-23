5931f55aed35dceb897cebd964d53dd0583ddfb0
Aniqa100 <anyutka.vs@gmail.com>
2022-11-23 20:41:20 +0300

    Signed-off-by: Aniqa100 <anyutka.vs@gmail.com>

--- cypress/support/page_objects/dropdownSelection.js
+++ cypress/support/page_objects/dropdownSelection.js
@@ -73,8 +73,8 @@ export class DropdownSelection{
     }
     InPapierkorbForm(value){
       cy.get('[akid="sProtokollInPapierkorbForm-geloeschtgrund"]').click()
-      .get('[class="select2-search select2-search--dropdown"]').type(value, {timeout: 1000})
-      .get('[class="select2-results__options"]').click();
+      cy.get('[class="select2-search select2-search--dropdown"]').type(value)
+      cy.get('[class="select2-results__options"]').click();
     }
     
   
